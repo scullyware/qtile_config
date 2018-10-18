8625863964386b90f34f49a7dcf0c4bd0fd919e3
scullyware <scullyware@ya.ru>
2018-10-18 15:06:31 +0300

    ddd

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+feh --bg-scale /home/gleb/wallpaper.jpg &
+#synclient TapButton2=3 &
+#synclient TapButton3=2 &
+#synclient TapButton1=1 &
+#source ~/.bash_aliases &
+#/home/gleb/bitmask-0.10.2/bitmask &
+firefox &
+xfce4-terminal &
+/home/gleb/Загрузки/Telegram/Telegram &
+volti &
+#skypeforlinux &
+#xfce4-power-manager &
+nm-applet start &
+
